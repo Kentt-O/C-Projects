c27a825fee3984bd8a34728c229eba2dc335e637
Kentt-O <kentc.okoro@gmail.com>
2024-06-02 06:04:36 +0100

    C++

---
+++ ChapterOne/TaskOne.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << " Introduction to Computers " << endl;
+    cout << " Welcome to object - Oriented Programming " << endl;
+
+    return 0;
+}
+
+// Created by Kent on 6/1/2024.
+//
---
+++ ChapterOne/TaskTwo.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 1; i <= 5; ++i) {
+        cout << " Welcome to C++ " << endl;
+    }
+}
+
+// Created by Kent on 6/1/2024.
