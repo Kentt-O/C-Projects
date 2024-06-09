ffb458cb5d39946c25f5945e0e94ce062917c203
Kentt-O <kentc.okoro@gmail.com>
2024-06-09 07:43:23 +0100

    Second commit

---
+++ ChapterOne/TaskFive.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "Program to complete and output the perimeter and "
+    << "area of a rectangle." << endl;
+
+    double length = 6.0;
+    double width = 4.0;
+    double perimeter = 2 * (length * width);
+    double area = length * width;
+
+    cout << "Length = " << length << endl;
+    cout << "Width = " << width << endl;
+    cout << "Perimeter = " << perimeter << endl;
+    cout << "Area = " << area << endl;
+
+    return 0;
+}
+// Created by Kent on 6/6/2024.
+//
---
+++ ChapterOne/TaskFour.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "9.5 x 4.5 - 2.5 x 3 / 45.5 - 3.5 = ";
+    cout << (9.5 * 4.5 - 2.5 * 3) / 45.5 - 3.5 << endl;
+
+    return 0;
+}
+// Created by Kent on 6/6/2024.
+//
